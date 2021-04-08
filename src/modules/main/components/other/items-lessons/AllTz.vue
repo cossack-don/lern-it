405b3586338cd0865800822a89b4e73c68b45dbd
SergeyZhur <ser90593696@yandex.ru>
2021-04-08 17:26:37 +0300

    corr

--- src/modules/main/components/other/items-lessons/AllTz.vue
+++ src/modules/main/components/other/items-lessons/AllTz.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="tz">
-    <a href="https://github.com/sknebo/test-task-JuniorDeveloper" target="_blank">ООО "Строительная компания "Небо web - разраб</a>
-    <a href="https://frontend-test.idaproject.com/" target="_blank" style="margin-left: 15px"
+    <a
+      href="https://github.com/sknebo/test-task-JuniorDeveloper"
+      target="_blank"
+      >ООО "Строительная компания "Небо web - разраб</a
+    >
+    <a
+      href="https://frontend-test.idaproject.com/"
+      target="_blank"
+      style="margin-left: 15px"
       >idaproject - nuxt/vue разраб</a
+    >
+    <a
+      href="https://vk.com/doc72176802_592915513?hash=6a766a23b35838ca02&dl=8f9c12fc1cc88c1ac4"
+      target="_blank"
+      style="margin-left: 15px"
+      >ТЗ от Лёни</a
     >
   </div>
 </template>
